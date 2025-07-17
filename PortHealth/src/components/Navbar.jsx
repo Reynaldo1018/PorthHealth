7b3fd7c2c2b861ba145a31a6b5cf907562eae2ec
Reynaldo1018 <reynaldoportela1018@icloud.com>
2025-07-16 23:24:31 -0400

    #

--- PortHealth/src/components/Navbar.jsx
+++ PortHealth/src/components/Navbar.jsx
@@ -7,77 +7,109 @@ const Navbar = () => {
     const navigate = useNavigate();
      const [showMenu, setShowMenu] = useState(false);
     const [token, setToken] = useState(true)
-       return (
+    return (
     <div 
      className=
-       'flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'
+    'flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'
      
      >
-        {/* <h1>PortHealth</h1> */}
+     {/* <h1>PortHealth</h1> */}
       {/* <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt=''/> */}
-       <h1 
-        onClick={() => navigate('/')} 
-        className='text-primary text-3xl font-bold cursor-pointer'
-       >
-        PortHealth Tele
+    <h1 
+     onClick={() => navigate('/')} 
+     className='text-primary text-3xl font-bold cursor-pointer'
+    >
+     PortHealth Tele
       </h1>
 
-        <ul className='hidden md:flex items-start gap-5 font-medium'>
-            {/* <NavLink to='/'>
-                <li className='py-1'>Home</li>
-                <hr className= {classhr}/>
-            </NavLink> */}
-            {/* <NavLink to='/doctors'>
-                <li className='py-1'>All Doctors</li>
-                <hr className= {classhr}/>
-            </NavLink> */}
-            {/* <NavLink to='/about'>
-                <li className='py-1'>About</li>
-                 <hr className= {classhr}/>
-            </NavLink> */}
-            {/* <NavLink to='/contact'>
-                <li className='py-1'>Contacts</li>
-                <hr className= {classhr}/>
-            </NavLink> */}
-        </ul>
-        <div className='flex items-center gap-4'>
-            {
-                token ?
-                <div className='flex items-center gap-2 cursor-pointer group relative'>
-                    <img 
-                     className='w-9 rounded-full'
-                     src={assets.profile_pic} alt=""
-                     />
-                     <img 
-                      className='w-2.5'
-                     src={assets.dropdown_icon} alt=''/>
-                     <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
-                         {/* bg-stone-100 color */}
-                        <div className='min-w-48 bg-white rounded flex flex-col gap-4 p-4 border-2 '>    
-                             <p 
-                              className={profileOptions}
-                              onClick={()=>navigate('/my-profile')}
-                             >Profile</p>
-                             <p 
-                              className={profileOptions} 
-                              onClick={()=>navigate('/my-appointments')}>Appointments</p>
-                             <p 
-                              className={profileOptions} 
-                              onClick={()=>setToken(false)}>Logout</p> 
-                        </div>
-                     </div>
-                </div> :
-                 <button 
-                onClick={()=>navigate('/login')}
-                className={caButtonStyle} 
-               >
-                Create Account
-            </button>
+     <ul className='hidden md:flex items-start gap-5 font-medium'>
+         {/* <NavLink to='/'>
+          <li className='py-1'>Home</li>
+          <hr className= {classhr}/>
+         </NavLink> */}
+         {/* <NavLink to='/doctors'>
+          <li className='py-1'>All Doctors</li>
+          <hr className= {classhr}/>
+         </NavLink> */}
+         {/* <NavLink to='/about'>
+          <li className='py-1'>About</li>
+           <hr className= {classhr}/>
+         </NavLink> */}
+         {/* <NavLink to='/contact'>
+          <li className='py-1'>Contacts</li>
+          <hr className= {classhr}/>
+         </NavLink> */}
+     </ul>
+     <div className='flex items-center gap-4'>
+         {
+          token ?
+          <div className='flex items-center gap-2 cursor-pointer group relative'>
+              <img 
+            className='w-9 rounded-full'
+            src={assets.profile_pic} alt=""
+            />
+            <img 
+             className='w-2.5'
+            src={assets.dropdown_icon} alt=''/>
+            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                {/* bg-stone-100 color */}
+               <div className='min-w-48 bg-white rounded flex flex-col gap-4 p-4 border-2 '>    
+                 <p 
+                  className={profileOptions}
+                  onClick={()=>navigate('/my-profile')}
+                 >Profile</p>
+                 <p 
+                  className={profileOptions} 
+                  onClick={()=>navigate('/my-appointments')}>Appointments</p>
+                 <p 
+                  className={profileOptions} 
+                  onClick={()=>setToken(false)}>Logout</p> 
+               </div>
+            </div>
+          </div> :
+           <button 
+          onClick={()=>navigate('/login')}
+          className={caButtonStyle} 
+         >
+          Create Account
+         </button>
 
-            }
-            <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt=''/>
-           
-        </div>
+         }
+         <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt=''/>
+         {/* Mobile Menu */}
+         <div className={`${showMenu ? `fixed w-full` : `h-0 w-0`} md:hidden right-0 top-0 z-20 overflow-hidden bg-white transition-all`}>
+          <div className='flex items-center justify-between px-5 py-6'>
+            <h1 
+              onClick={() => navigate('/')} 
+              className='text-primary text-3xl font-bold cursor-pointer'
+              
+            >
+                PortHealth Tele
+            </h1>
+              <svg 
+             className='w-7 h-7 cursor-pointer text-primary' 
+             onClick={() => setShowMenu(false)} 
+             xmlns="http://www.w3.org/2000/svg" 
+             fill="none" 
+             viewBox="0 0 24 24" 
+             stroke="currentColor"
+              >
+             <path 
+               strokeLinecap="round" 
+               strokeLinejoin="round" 
+               strokeWidth={2} 
+               d="M6 18L18 6M6 6l12 12" 
+             />
+              </svg>
+          </div>
+          <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+              <NavLink onClick={()=>setShowMenu(false)} to='/'>Home</NavLink>
+              <NavLink onClick={()=>setShowMenu(false)} to='/doctors'>All Providers</NavLink>
+              {/* <NavLink>About</NavLink> */}
+              {/* <NavLink>Contact</NavLink> */}
+          </ul>
+         </div>
+     </div>
     </div>
   )
 }
